46358b38a284448ea86a54660c6d8ac152487981
root <root@job-fa30cbb6-7578-4c65-ba38-897a73eb53c2-r87ft>
2024-03-19 02:30:16 +0000

    Moved Stripe payment link creation to dashboard/views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,13 @@
 import markdown
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.utils.safestring import mark_safe
 from django.views.generic import DetailView
 from django_tables2 import SingleTableView
+from django.urls import reverse
+import stripe
+from django.conf import settings
 
 from accounts.models import UserBudget
 from dashboard.tables import TaskTable, EventTable, CostItemTable
@@ -64,3 +67,28 @@ class TaskDetailView(LoginRequiredMixin, DetailView):
         context['task_result'] = mark_safe(markdown.markdown(task.result))
         context['total_cost'] = sum([item.credits for item in task.cost_items.all()])
         return context
+
+@csrf_exempt
+def create_stripe_payment_link(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('account_login'))
+
+    stripe.api_key = settings.STRIPE_API_KEY
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': 'Credits Refill',
+                },
+                'unit_amount': 100,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=request.build_absolute_uri('/') + '?success=true',
+        cancel_url=request.build_absolute_uri('/') + '?canceled=true',
+        metadata={'user_id': request.user.id},
+    )
+    return HttpResponseRedirect(session.url)
