c96bbd89a382e321cdfbcb1a0d5e026aab02dfd7
Marco Lamina <mlamina09@gmail.com>
2024-04-11 15:50:41 -0700

    Remove unused classes

--- engine/prompt.py
+++
@@ -1,6 +0,0 @@
-class Prompt:
-    def __init__(self, message: str):
-        self.message = message
-
-    def __str__(self):
-        return self.message
--- engine/task_result.py
+++
@@ -1,8 +0,0 @@
-class TaskResult:
-    def __init__(self, success: bool, message: str = None, data: dict = None):
-        self.success = success
-        self.message = message
-        self.data = data if data is not None else {}
-
-    def __str__(self):
-        return f"Success: {self.success}, Message: {self.message}, Data: {self.data}"
