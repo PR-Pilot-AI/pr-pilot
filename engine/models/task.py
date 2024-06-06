6867f1914e15ba1cdc39b6a410a0ed1b0a63ae69
Marco Lamina <mlamina09@gmail.com>
2024-06-06 16:55:31 -0700

    Increase integer range for comment_id on Task model

--- engine/models/task.py
+++ engine/models/task.py
@@ -43,7 +43,7 @@ class Task(models.Model):
     user_request = models.TextField(blank=True)
     head = models.CharField(max_length=200, blank=True, null=True)
     base = models.CharField(max_length=200, blank=True, null=True)
-    comment_id = models.IntegerField(null=True)
+    comment_id = models.BigIntegerField(null=True)
     comment_url = models.CharField(max_length=200, blank=True, null=True)
     response_comment_id = models.IntegerField(blank=True, null=True)
     response_comment_url = models.CharField(max_length=200, blank=True, null=True)
