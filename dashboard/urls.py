1f5c3ab84202a3dd0ec45bbb79fc7b2de5c83c0a
Marco Lamina <mlamina09@gmail.com>
2024-06-10 17:55:16 -0700

    Add unit tests

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,7 +6,11 @@ import dashboard.views.stripe
 import dashboard.views.tasks
 
 urlpatterns = [
-    path("refill/", dashboard.views.stripe.create_stripe_payment_link, name="refill_budget"),
+    path(
+        "refill/",
+        dashboard.views.stripe.create_stripe_payment_link,
+        name="refill_budget",
+    ),
     path(
         "add-slack-integration/",
         dashboard.views.integrations.add_slack_integration,
@@ -18,8 +22,24 @@ urlpatterns = [
         name="add_linear_integration",
     ),
     path("tasks/", dashboard.views.tasks.TaskListView.as_view(), name="task_list"),
-    path("integrations/", dashboard.views.integrations.IntegrationView.as_view(), name="integrations"),
-    path("api-keys/", dashboard.views.api_keys.APIKeyListView.as_view(), name="api_key_list"),
-    path("tasks/<uuid:pk>/", dashboard.views.tasks.TaskDetailView.as_view(), name="task_detail"),
-    path("tasks/<uuid:pk>/undo/", dashboard.views.tasks.TaskUndoView.as_view(), name="task_undo"),
+    path(
+        "integrations/",
+        dashboard.views.integrations.IntegrationView.as_view(),
+        name="integrations",
+    ),
+    path(
+        "api-keys/",
+        dashboard.views.api_keys.APIKeyListView.as_view(),
+        name="api_key_list",
+    ),
+    path(
+        "tasks/<uuid:pk>/",
+        dashboard.views.tasks.TaskDetailView.as_view(),
+        name="task_detail",
+    ),
+    path(
+        "tasks/<uuid:pk>/undo/",
+        dashboard.views.tasks.TaskUndoView.as_view(),
+        name="task_undo",
+    ),
 ]
